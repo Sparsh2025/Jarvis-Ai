e3660fe1c6caa0438d00ec976ff12ca544d5802f
Sparsh2025 <sparshchoubey19@gmail.com>
2025-11-07 11:34:56 +0530

    Update

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -22,11 +22,7 @@ async function authRegister(req,res){
         password:bcryptPassword
     })
     const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
-    res.cookie('token',token,{
-         httpOnly: true,
-         sameSite: "strict",
-         path: "/",
-    })
+    res.cookie('token',token)
     res.status(201).json({
         message:"user created",
         user
@@ -55,11 +51,7 @@ async function authLogin(req,res){
         })
     }
     const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
-    res.cookie('token',token,{
-         httpOnly: true,
-         sameSite: "strict",
-         path: "/",
-    })
+    res.cookie('token',token)
     res.status(200).json({
         message:"login success",
         user
@@ -68,11 +60,7 @@ async function authLogin(req,res){
 
  function logout(req,res){
   try { 
-      res.clearCookie("token", {
-      httpOnly: true,
-      sameSite: "strict",
-      path: "/",
-    });
+      res.clearCookie("token");
 
     return res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
